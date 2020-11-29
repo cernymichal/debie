83198ace2df6003c078f615b49f7143098da01a7
Michal Černý <cernyymichal@gmail.com>
2020-11-29 23:04:18 +0100

    page layout, add lato and opensans

---
+++ Models/Crumb.cs
@@ -0,0 +1,7 @@
+namespace Debie.Models {
+    public class Crumb {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Title { get; set; }
+    }
+}
