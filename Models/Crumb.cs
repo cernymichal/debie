96b0564c63f047cfa65ece0a5ce716f50ed9cb76
Michal Černý <cernyymichal@gmail.com>
2020-11-30 23:25:39 +0100

    image header

--- Models/Crumb.cs
+++ Models/Crumb.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+
 namespace Debie.Models {
     public class Crumb {
         public string Controller { get; set; }
         public string Action { get; set; }
         public string Title { get; set; }
+        public IDictionary<string, string> Values { get; set; }
+
+        public Crumb(string controller, string action, string title, IDictionary<string, string> values = null) {
+            Controller = controller;
+            Action = action;
+            Title = title;
+            Values = values;
+        }
     }
 }
